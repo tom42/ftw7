794eeedf8f12df72e5a54ac28777cb1fdb5b1a28
tom42 <tom42@server.fake>
2014-07-23 21:00:01 +0200

    ftw: minimal command line parser using argp.

--- ftw7/ftw7/main.cpp
+++ ftw7/ftw7/main.cpp
@@ -1,3 +1,39 @@
-int main()
+/*
+ * Copyright 2012-2014 Thomas Mathys
+ *
+ * This file is part of ftw7.
+ *
+ * ftw7 is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ftw7 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ftw7.If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <cstdlib>
+#include <iostream>
+#include "argp/argp.h"
+
+static void parse_command_line(int argc, char *argv[])
 {
+    argp_parse(nullptr, argc, argv, 0, nullptr, nullptr);
+}
+
+int main(int argc, char *argv[])
+{
+    try
+    {
+        parse_command_line(argc, argv);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << argv[0] << ": " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 }
