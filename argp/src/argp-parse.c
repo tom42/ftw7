985c5d5e8a6baeef72cf90f37d3b07dc71bfdb54
tom42 <tom42@server.fake>
2014-07-23 13:16:17 +0200

    argp-parse.c: include unistd.h only if it's available.

--- argp/src/argp-parse.c
+++ argp/src/argp-parse.c
@@ -39,7 +39,9 @@ char *alloca ();
 
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#if defined(HAVE_UNISTD_H) && HAVE_UNISTD_H
+# include <unistd.h>
+#endif
 #include <limits.h>
 #include <getopt.h>
 #include <getopt_int.h>
