fe53043d8dfd8ff9748f077d4e74b31628ff4052
tom42 <tom42@server.fake>
2014-07-23 13:35:57 +0200

    Fixed conditional compilation that led to a non-working attempt to use strerror_r with MSVC.

--- argp/src/argp-help.c
+++ argp/src/argp-help.c
@@ -1868,7 +1868,7 @@ __argp_failure (const struct argp_state *state, int status, int errnum,
 #else
 	      putc_unlocked (':', stream);
 	      putc_unlocked (' ', stream);
-# ifdef HAVE_STRERROR_R
+# if defined(HAVE_STRERROR_R) && HAVE_STRERROR_R
 	      fputs (__strerror_r (errnum, buf, sizeof (buf)), stream);
 # else
 	      fputs (strerror (errnum), stream);
