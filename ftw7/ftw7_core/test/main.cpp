52a89e3092ccc41e89edc2d4b317345f58598453
tom42 <tom42@server.fake>
2014-07-27 19:56:12 +0200

    Prepared ftw7_core_test

---
+++ ftw7/ftw7_core/test/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2012-2014 Thomas Mathys
+ *
+ * This file is part of ftw7.
+ *
+ * ftw7 is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ftw7 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ftw7.If not, see <http://www.gnu.org/licenses/>.
+ */
+#define BOOST_TEST_MODULE ftw7_unit_test
+#include <boost/test/unit_test.hpp>
+
+// This file intentionally doesn't contain any tests.
+// Its sole purpose is to generate the test runner, that is, main().
