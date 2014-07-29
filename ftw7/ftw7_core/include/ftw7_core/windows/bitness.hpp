7c3fef85a3c84896c8a2ce12c0898a790e046b32
tom42 <tom42@users.noreply.github.com>
2014-07-29 18:40:46 +0200

    Added wow64.cpp and bitness.cpp from prototype II, merged into a single module (bitness.cpp)

---
+++ ftw7/ftw7_core/include/ftw7_core/windows/bitness.hpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2012-2014 Thomas Mathys
+ *
+ * This file is part of ftw7.
+ *
+ * ftw7 is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ftw7 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ftw7.If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef FTW7_CORE_WINDOWS_BITNESS_HPP_INCLUDED
+#define FTW7_CORE_WINDOWS_BITNESS_HPP_INCLUDED
+
+#include <Windows.h>
+
+namespace ftw7_core
+{
+namespace windows
+{
+
+bool is_wow64_process();
+bool is_wow64_process(HANDLE process);
+bool is_64bit_os();
+bool is_64bit_process();
+bool is_64bit_process(HANDLE process);
+
+}
+}
+
+#endif
---
+++ ftw7/ftw7_core/src/bitness.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012-2014 Thomas Mathys
+ *
+ * This file is part of ftw7.
+ *
+ * ftw7 is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ftw7 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ftw7.If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ftw7_core/wexcept.hpp"
+#include "ftw7_core/windows/bitness.hpp"
+#include "ftw7_core/windows/format_message.hpp"
+#include "ftw7_core/windows/module.hpp"
+
+namespace ftw7_core
+{
+namespace windows
+{
+
+bool is_wow64_process()
+{
+    return is_wow64_process(GetCurrentProcess());
+}
+
+bool is_wow64_process(HANDLE process)
+{
+    // TODO: use decltype and get a correct function pointer automatically?
+    typedef BOOL (WINAPI *IsWow64ProcessPtr)(HANDLE, PBOOL);
+    HMODULE kernel32 = get_module_handle(L"kernel32.dll");
+
+    IsWow64ProcessPtr isWow64Process = 
+        reinterpret_cast<IsWow64ProcessPtr>(GetProcAddress(kernel32,
+            "IsWow64Process"));
+    if (!isWow64Process)
+    {
+        // IsWow64Process is not available, so Wow64 can't be available either.
+        return false;
+    }
+
+    BOOL iswow64 = FALSE;
+    if (!isWow64Process(process, &iswow64))
+    {
+        const DWORD error = GetLastError();
+        // TODO: why not windows_error?
+        throw wruntime_error(L"IsWow64Process failed: " +
+            wformat_message_from_system(error));
+    }
+    return iswow64 ? true : false;
+}
+
+}
+}
