c329178dfbe02c0cbdad780a032ed728c15461ce
tom42 <tom42@users.noreply.github.com>
2014-08-03 08:47:05 +0200

    settings::initialize: using a static_assert to ensure that PACKAGE_STRING fits into magic_string.

--- ftw7/ftw7_core/src/emulation.cpp
+++ ftw7/ftw7_core/src/emulation.cpp
@@ -30,12 +30,8 @@ void settings::initialize(settings& settings)
     memset(&settings, 0, sizeof(settings));
     settings.size = sizeof(settings);
 
-    // TODO: nasty, really.
-    // * We should either at compile or runtime ensure that PACKAGE_STRING fits
-    //   into magic_string. If not we fail compilation or execution. I can
-    //   totally live with both. Just clamping PACKAGE_STRING by using strncpy
-    //   is not nice, tho.
-    strncpy(settings.magic_string, PACKAGE_STRING, sizeof(settings.magic_string));
+    static_assert(sizeof(PACKAGE_STRING) <= sizeof(settings.magic_string), "PACKAGE_STRING is too long");
+    strcpy(settings.magic_string, PACKAGE_STRING);
 }
 
 }
