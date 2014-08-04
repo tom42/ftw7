102f0daba7142e632e86c99055587b0178337289
tom42 <tom42@users.noreply.github.com>
2014-08-04 20:36:16 +0200

    Added base class for display drivers.

---
+++ ftw7/ftw7_conemu/include/ftw7_conemu/emulation/display_driver.hpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2012-2014 Thomas Mathys
+ *
+ * This file is part of ftw7.
+ *
+ * ftw7 is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ftw7 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ftw7.If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef FTW7_CONEMU_EMULATION_DISPLAY_DRIVER_HPP_INCLUDED
+#define FTW7_CONEMU_EMULATION_DISPLAY_DRIVER_HPP_INCLUDED
+
+namespace ftw7_conemu
+{
+namespace emulation
+{
+
+class display_driver
+{
+public:
+    virtual ~display_driver() {}
+};
+
+}
+}
+
+#endif
