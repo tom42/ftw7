ffb05edaa3d1c26b29cb220131560b15a00f354f
tom42 <tom42@users.noreply.github.com>
2014-08-11 07:50:57 +0200

    Added 8x8 VGA ROM font.

---
+++ ftw7/ftw7_conemu/include/ftw7_conemu/display/vga8x8.hpp
@@ -0,0 +1,10 @@
+/*
+* VGA 8x8 ROM font.
+* Taken from fntcol16.zip.
+*/
+#ifndef FTW7_CONEMU_DISPLAY_VGA8X8_HPP_INCLUDED
+#define FTW7_CONEMU_DISPLAY_VGA8X8_HPP_INCLUDED
+
+extern "C" const unsigned char vga8x8[];
+
+#endif
