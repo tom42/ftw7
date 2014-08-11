4b9bc9a327d67fae6b842637449c1b434fdb1f17
tom42 <tom42@users.noreply.github.com>
2014-08-11 16:42:26 +0200

    Added skeleton for opengl display driver.

---
+++ ftw7/ftw7_conemu/include/ftw7_conemu/display/opengl_display_driver.hpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012-2014 Thomas Mathys
+ *
+ * This file is part of ftw7.
+ *
+ * ftw7 is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ftw7 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ftw7.If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef FTW7_CONEMU_DISPLAY_OPENGL_DISPLAY_DRIVER_HPP_INCLUDED
+#define FTW7_CONEMU_DISPLAY_OPENGL_DISPLAY_DRIVER_HPP_INCLUDED
+
+#include <Windows.h>
+#include "ftw7_core/emulation/emulation.hpp"
+#include "ftw7_conemu/display/display_driver.hpp"
+
+namespace ftw7_conemu
+{
+namespace display
+{
+
+class opengl_display_driver : public display_driver
+{
+public:
+    opengl_display_driver(HINSTANCE emulation_dll_module_handle, const ftw7_core::emulation::settings& settings);
+    virtual ~opengl_display_driver();
+private:
+    opengl_display_driver(const opengl_display_driver&) = delete;
+    opengl_display_driver& operator = (const opengl_display_driver&) = delete;
+};
+
+}
+}
+
+#endif
