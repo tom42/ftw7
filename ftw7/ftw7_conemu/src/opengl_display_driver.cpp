829a3677d9cac5ee469f40b368900d8e87a30c4a
tom42 <tom42@users.noreply.github.com>
2014-08-11 19:14:46 +0200

    opengl_display_driver: added key callback and test rendering code.

--- ftw7/ftw7_conemu/src/opengl_display_driver.cpp
+++ ftw7/ftw7_conemu/src/opengl_display_driver.cpp
@@ -37,6 +37,14 @@ void error_callback(int /*error*/, const char* description)
     FTW7_LOG_ERROR << L"glfw: " << description;
 }
 
+void key_callback(GLFWwindow* window, int key, int /*scancode*/, int action, int /*mods*/)
+{
+    if ((key == GLFW_KEY_ESCAPE) && (action == GLFW_PRESS))
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 }
 
 opengl_display_driver::opengl_display_driver(HINSTANCE /*emulation_dll_module_handle*/, const ftw7_core::emulation::settings& /*settings*/)
@@ -52,6 +60,7 @@ opengl_display_driver::opengl_display_driver(HINSTANCE /*emulation_dll_module_ha
     glfwInit();
     m_window = glfwCreateWindow(640, 400, FTW7_OPENGL_DISPLAY_DRIVER_NAME, nullptr, nullptr);
     glfwMakeContextCurrent(m_window);
+    glfwSetKeyCallback(m_window, key_callback);
 }
 
 opengl_display_driver::~opengl_display_driver()
@@ -67,7 +76,29 @@ bool opengl_display_driver::handle_messages()
 
 void opengl_display_driver::render(const CHAR_INFO* /*buffer*/)
 {
-    // TODO: render and swap buffers
+    // TODO: real rendering code
+    float ratio;
+    int width, height;
+
+    glfwGetFramebufferSize(m_window, &width, &height);
+    ratio = width / (float)height;
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+    glEnd();
+    glfwSwapBuffers(m_window);
 }
 
 void opengl_display_driver::set_title(const wchar_t* title)
