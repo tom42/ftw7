5ae4e6ac71a2e15c7a30d654a10085e387d8fb34
tom42 <tom42@users.noreply.github.com>
2014-08-19 08:35:11 +0200

    Moved function pointer definitions into own header again.
    win32_function_pointer_types.h needs to be manually maintained, but
    hooked_functions.h should really be generated.

--- ftw7/ftw7_conemu/include/ftw7_conemu/emulation/hooked_functions.h
+++ ftw7/ftw7_conemu/include/ftw7_conemu/emulation/hooked_functions.h
@@ -19,52 +19,17 @@
 #ifndef FTW7_CONEMU_EMULATION_HOOKED_FUNCTIONS_H_INCLUDED
 #define FTW7_CONEMU_EMULATION_HOOKED_FUNCTIONS_H_INCLUDED
 
+#include "win32_function_pointer_types.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/*
- * Hack for GLFW:
- *
- * GLFW insists in not including Windows.h but defines its own symbols and
- * messes around with WIN32_LEAN_AND_MEAN. This leads of course to the usual
- * mess, in this case a ton of annoying warnings about redefinitions of
- * WIN32_LEAN_AND_MEAN, and thus we came up with the following hack:
- *
- * When GLFW is compiled we include its internal.h rather than Windows.h,
- * and everything compiles without any warnings.
- *
- * This should be pretty much OK, since the first thing in the GLFW sources
- * is the inclusion of internal.h. So if we introduce a prologue header
- * that includes internal.h before doing anything else, then we shouldn't
- * have changed anything from the compiler's point of view.
- */
 #ifdef FTW7_COMPILING_GLFW
-#include "internal.h"
 // TODO: Hackage. Anyway, at this point we can stop using xheaders and write a generator instead.
 #define ShowCursor true_ShowCursor
 #define ShowWindow true_ShowWindow
-#else
-#include <Windows.h>
 #endif
-
-
-/******************************************************************************
- * kernel32 function pointer types
- ******************************************************************************/
-typedef BOOL(WINAPI* AllocConsole_ptr_t)(void);
-typedef BOOL(WINAPI* SetConsoleActiveScreenBuffer_ptr_t)(HANDLE);
-typedef BOOL(WINAPI* SetConsoleTitleA_ptr_t)(LPCSTR);
-typedef BOOL(WINAPI* SetConsoleTitleW_ptr_t)(LPCWSTR);
-typedef BOOL(WINAPI* WriteConsoleOutputA_ptr_t)(HANDLE, const CHAR_INFO*, COORD, COORD, PSMALL_RECT);
-
-
-/******************************************************************************
-* user32 function pointer types
-******************************************************************************/
-typedef int (WINAPI* ShowCursor_ptr_t)(BOOL);
-typedef BOOL(WINAPI* ShowWindow_ptr_t)(HWND, int);
-
 
 /*
  * Create declarations for the variables that hold the addresses of the true functions.
