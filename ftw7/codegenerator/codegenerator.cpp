5636f070c945dfa2289e5255add6c3e7b88f95a4
tom42 <tom42@users.noreply.github.com>
2014-08-19 11:43:14 +0200

    codegenerator generates now also function pointer typedefs
    Rationale: one file less to maintain, and less DRY principle violation
    since the master list is now the hooked_functions array inside
    codegenerator.cpp.

--- ftw7/codegenerator/codegenerator.cpp
+++ ftw7/codegenerator/codegenerator.cpp
@@ -24,17 +24,18 @@ struct hooked_function
 {
     const char* dllname;
     const char* procname;
+    const char* function_pointer_typedef;
 };
 
 const hooked_function hooked_functions[] =
 {
-    { "kernel32", "AllocConsole" },
-    { "kernel32", "SetConsoleActiveScreenBuffer" },
-    { "kernel32", "SetConsoleTitleA" },
-    { "kernel32", "SetConsoleTitleW" },
-    { "kernel32", "WriteConsoleOutputA" },
-    { "user32", "ShowCursor" },
-    { "user32", "ShowWindow" },
+    { "kernel32",   "AllocConsole",                 "typedef BOOL(WINAPI* AllocConsole_ptr_t)(void);" },
+    { "kernel32",   "SetConsoleActiveScreenBuffer", "typedef BOOL(WINAPI* SetConsoleActiveScreenBuffer_ptr_t)(HANDLE);" },
+    { "kernel32",   "SetConsoleTitleA",             "typedef BOOL(WINAPI* SetConsoleTitleA_ptr_t)(LPCSTR);" },
+    { "kernel32",   "SetConsoleTitleW",             "typedef BOOL(WINAPI* SetConsoleTitleW_ptr_t)(LPCWSTR);" },
+    { "kernel32",   "WriteConsoleOutputA",          "typedef BOOL(WINAPI* WriteConsoleOutputA_ptr_t)(HANDLE, const CHAR_INFO*, COORD, COORD, PSMALL_RECT);" },
+    { "user32",     "ShowCursor",                   "typedef int (WINAPI* ShowCursor_ptr_t)(BOOL);" },
+    { "user32",     "ShowWindow",                   "typedef BOOL(WINAPI* ShowWindow_ptr_t)(HWND, int);" },
 };
 const size_t n_hooked_functions = sizeof(hooked_functions) / sizeof(hooked_functions[0]);
 
@@ -42,15 +43,33 @@ void generate_hooked_functions_header(std::ostream& os)
 {
     // TODO: proper header guard (ideally generated from output filename...)
     os << "/* Generated source code. Do not modify. */" << std::endl;
-    os << "#ifndef FTGW7_RULEZ_H_INCLUDED" << std::endl;
-    os << "#define FTGW7_RULEZ_H_INCLUDED" << std::endl;
+    os << "#ifndef FTW7_RULEZ_H_INCLUDED" << std::endl;
+    os << "#define FTW7_RULEZ_H_INCLUDED" << std::endl;
     os << std::endl;
 
-    os << "#include \"win32_function_pointer_types.h\"" << std::endl;
+    // Hack for GLFW:
+    //
+    // GLFW insists in not including Windows.h but defines its own symbols and
+    // messes around with WIN32_LEAN_AND_MEAN. This leads of course to the usual
+    // mess, in this case a ton of annoying warnings about redefinitions of
+    // WIN32_LEAN_AND_MEAN, and thus we came up with the following hack:
+    //
+    // When GLFW is compiled we include its internal.h rather than Windows.h,
+    // and everything compiles without any warnings.
+    //
+    // This should be pretty much OK, since the first thing in the GLFW sources
+    // is the inclusion of internal.h. So if we introduce a prologue header
+    // that includes internal.h before doing anything else, then we shouldn't
+    // have changed anything from the compiler's point of view.
+    os << "#ifdef FTW7_COMPILING_GLFW" << std::endl;
+    os << "#include \"internal.h\"" << std::endl;
+    os << "#else" << std::endl;
+    os << "#include <Windows.h>" << std::endl;
+    os << "#endif" << std::endl;
     os << std::endl;
 
     os << "#ifdef __cplusplus" << std::endl;
-    os << "#extern \"C\" {" << std::endl;
+    os << "extern \"C\" {" << std::endl;
     os << "#endif" << std::endl;
     os << std::endl;
 
@@ -62,6 +81,13 @@ void generate_hooked_functions_header(std::ostream& os)
             hooked_functions[i].procname << ";" << std::endl;
     }
     os << "#endif" << std::endl;
+    os << std::endl;
+
+    // Generate function pointer typedefs.
+    for (size_t i = 0; i < n_hooked_functions; ++i)
+    {
+        os << hooked_functions[i].function_pointer_typedef << std::endl;
+    }
     os << std::endl;
 
     // Generate extern declarations for true function pointers.
